d6800c94e7a28ff9c71d5c020e6c7c1213397ff9
Eric Brown <ebrown22@bu.edu>
2023-05-28 12:32:54 -0400

    do week3 with pool and starmap

--- w3/main.py
+++ w3/main.py
@@ -145,7 +145,7 @@ def main() -> List[Dict]:
     """
 
     st = time.time()
-    n_processes = 3 # you may modify this number - check out multiprocessing.cpu_count() as well
+    n_processes = 8
 
     parser = argparse.ArgumentParser(description="Choose from one of these : [tst|sml|bg]")
     parser.add_argument('--type',
@@ -179,7 +179,16 @@ def main() -> List[Dict]:
     # for process in processes:
     #     process.join()
 
-    # TODO: do the same thing using `pool` and `starmap `
+    # Now do the same thing using `pool` and `starmap `:
+    
+    with multiprocessing.Pool(processes=n_processes)as pool:
+        revenue_data = pool.starmap(run, [(batch, n_process) for n_process, batch in enumerate(batches)] )
+        revenue_data = flatten(lst=revenue_data)
+
+        pool.close()
+        pool.join()
+
+
 
 
     ######################################## YOUR CODE HERE ##################################################
@@ -188,9 +197,17 @@ def main() -> List[Dict]:
     print("Overall time taken : {}".format(en-st))
 
     # should return revenue data
-    return [{}]
+    # return [{}]
+    for yearly_data in revenue_data:
+        with open(os.path.join(output_save_folder, f'{yearly_data["file_name"]}.json'), 'w') as f:
+            f.write(json.dumps(yearly_data))
+
+        plot_sales_data(yearly_revenue=yearly_data['revenue_per_region'], year=yearly_data["file_name"],
+                        plot_save_path=os.path.join(output_save_folder, f'{yearly_data["file_name"]}.png'))
+
+    return revenue_data
 
 
 if __name__ == '__main__':
     res = main()
-    pprint(res)
+    # pprint(res)
